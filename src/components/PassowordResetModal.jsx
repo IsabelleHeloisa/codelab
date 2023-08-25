17ed7ed77db25c2542c36cb6cd0ba6bf8634f22e
IsabelleHeloisa <isabelle.hello123@outlook.com>
2023-08-24 22:09:35 -0300

    feat: toggle show password

--- src/components/PassowordResetModal.jsx
+++ src/components/PassowordResetModal.jsx
@@ -5,9 +5,11 @@ import {
   DialogContent,
   DialogContentText,
   DialogTitle,
-  TextField
+  FormControl
 } from '@mui/material'
 import React from 'react'
+
+import InputPassword from './InputPassword'
 
 export default function PasswordResetModal({ open = false, handleClose }) {
   return (
@@ -15,24 +17,11 @@ export default function PasswordResetModal({ open = false, handleClose }) {
       <DialogTitle>Alterar Senha</DialogTitle>
       <DialogContent>
         <DialogContentText>Digite a nova senha</DialogContentText>
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Nova Senha"
-          type="password"
-          fullWidth
-          variant="standard"
-        />
-        <TextField
-          autoFocus
-          margin="dense"
-          id="name"
-          label="Confirmar nova Senha"
-          type="password"
-          fullWidth
-          variant="standard"
-        />
+
+        <FormControl variant="standard">
+          <InputPassword label="Nova Senha" />
+          <InputPassword label="Confirmar Senha" />
+        </FormControl>
       </DialogContent>
       <DialogActions>
         <Button onClick={handleClose}>Cancelar</Button>
